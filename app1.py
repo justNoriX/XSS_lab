d46b6afeb978f94acfeb47ef2a06c94ab7e6625a
justNoriX <osur@onet.pl>
2026-04-30 07:51:27 -0400

    fix

--- app1.py
+++ app1.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 import html
+import os
 
 app = Flask(__name__)
 
@@ -313,4 +314,5 @@ def reflected():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
